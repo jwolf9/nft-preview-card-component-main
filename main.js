4b8fddffe2ab1dcb5ab83c2f27ea2ceb422064fa
jonathan <jonathan@mobagel.com>
2022-01-26 16:40:42 +0800

    add mask for highlighting image

--- main.js
+++ main.js
@@ -1,10 +1,13 @@
 const title = document.querySelector('.nft-title');
 const creatorName = document.querySelector('.name')
+const image = document.querySelector('.equilibrium')
 
 title.addEventListener('mouseover', highlight);
 title.addEventListener('mouseout', unhighlight);
 creatorName.addEventListener('mouseover', highlight);
 creatorName.addEventListener('mouseout', unhighlight)
+
+equilibrium.addEventListener('mousover', highlight);
 
 function highlight(e) {
     e.target.style.color = 'turquoise';
