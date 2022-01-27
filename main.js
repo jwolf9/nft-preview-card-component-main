0936aff8a5cf4120b1b30eee11abaf5790cd69fa
jonathan <jonathan@mobagel.com>
2022-01-27 09:55:39 +0800

    added highlighting for image

--- main.js
+++ main.js
@@ -1,13 +1,26 @@
 const title = document.querySelector('.nft-title');
-const creatorName = document.querySelector('.name')
-const image = document.querySelector('.equilibrium')
+const creatorName = document.querySelector('.name');
+const mask = document.querySelector('.mask');
+const eye = document.querySelector('.eye');
+const equilibrium = document.querySelector('.container');
 
 title.addEventListener('mouseover', highlight);
 title.addEventListener('mouseout', unhighlight);
 creatorName.addEventListener('mouseover', highlight);
-creatorName.addEventListener('mouseout', unhighlight)
+creatorName.addEventListener('mouseout', unhighlight);
 
-equilibrium.addEventListener('mousover', highlight);
+equilibrium.addEventListener('mouseover', activateImage);
+equilibrium.addEventListener('mouseout', deactivateImage);
+
+function activateImage(e) {
+    mask.style.display = 'block';
+    eye.style.display = 'block';
+}
+
+function deactivateImage(e) {
+    mask.style.display = 'none';
+    eye.style.display = 'none';
+}
 
 function highlight(e) {
     e.target.style.color = 'turquoise';
